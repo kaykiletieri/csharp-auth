9799cbfd2c4f3cf6c8197815f9bc1b88bd4421f4
Kayki Letieri <kaykiletieri37@gmail.com>
2024-05-14 13:40:32 -0300

    feat: add initial infrastructure

---
+++ Infrastructure/Extensions/DataExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CSharpAuth.Infrastructure.Repositories;
+using CSharpAuth.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpAuth.Infrastructure.Extensions;
+
+public static class DataExtensions
+{
+    public static IServiceCollection AddEntityFramework(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+
+        return services;
+    }
+}
---
+++ Infrastructure/Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using CSharpAuth.Domain.Entities;
+
+namespace CSharpAuth.Infrastructure.Repositories.Interfaces;
+
+public interface IRepositoryBase<TEntity> where TEntity : EntityBase
+{
+    Task<IEnumerable<TEntity>> GetAllActiveAsync();
+    Task<TEntity?> GetActiveByIdAsync(Guid uuid);
+    Task<TEntity> CreateAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task SoftDeleteAsync(Guid uuid);
+}
