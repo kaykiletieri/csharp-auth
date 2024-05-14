cae0d7e524054ca8809dc088125aa78854b1fffe
Kayki Letieri <kaykiletieri37@gmail.com>
2024-05-14 13:41:42 -0300

    feat: add initial entities

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpAuth.Domain.Entities;
+public class User : EntityBase
+{
+    public required string Username { get; set; }
+    public required string HashedPassword { get; set; }
+    public required byte[] Salt { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; } = [];
+}
