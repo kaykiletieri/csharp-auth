dba4c6a2f1131ab7ddf03dabec635d0ef4c0692b
Kayki Letieri <kaykiletieri37@gmail.com>
2024-05-14 13:45:11 -0300

    feat: map entities

---
+++ Infrastructure/Mappings/PermissionMapping.cs
@@ -0,0 +1,41 @@
+﻿using CSharpAuth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CSharpAuth.Infrastructure.Mappings;
+
+public class PermissionMapping : IEntityTypeConfiguration<Permission>
+{
+    public void Configure(EntityTypeBuilder<Permission> builder)
+    {
+        builder.ToTable("permissions");
+
+        builder.HasKey(p => p.Uuid);
+
+        builder.Property(p => p.Uuid)
+            .HasColumnName("uuid")
+            .IsRequired();
+
+        builder.Property(p => p.Name)
+            .HasColumnName("name")
+            .IsRequired();
+
+        builder.Property(p => p.Description)
+            .HasColumnName("description");
+
+        builder.Property(p => p.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+
+        builder.Property(p => p.UpdatedAt)
+            .HasColumnName("updated_at")
+            .IsRequired();
+
+        builder.Property(p => p.DeletedAt)
+            .HasColumnName("deleted_at");
+
+        builder.HasMany(p => p.RolePermissions)
+            .WithOne(rp => rp.Permission)
+            .HasForeignKey(rp => rp.PermissionUuid);
+    }
+}
---
+++ Infrastructure/Mappings/RoleMapping.cs
@@ -0,0 +1,45 @@
+﻿using CSharpAuth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CSharpAuth.Infrastructure.Mappings;
+
+public class RoleMapping : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("roles");
+
+        builder.HasKey(r => r.Uuid);
+
+        builder.Property(r => r.Uuid)
+            .HasColumnName("uuid")
+            .IsRequired();
+
+        builder.Property(r => r.Name)
+            .HasColumnName("name")
+            .IsRequired();
+
+        builder.Property(r => r.Description)
+            .HasColumnName("description");
+
+        builder.Property(r => r.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+
+        builder.Property(r => r.UpdatedAt)
+            .HasColumnName("updated_at")
+            .IsRequired();
+
+        builder.Property(r => r.DeletedAt)
+            .HasColumnName("deleted_at");
+
+        builder.HasMany(r => r.RolePermissions)
+            .WithOne(rp => rp.Role)
+            .HasForeignKey(rp => rp.RoleUuid);
+
+        builder.HasMany(r => r.UserRoles)
+            .WithOne(ur => ur.Role)
+            .HasForeignKey(ur => ur.RoleUuid);
+    }
+}
