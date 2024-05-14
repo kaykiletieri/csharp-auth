f0296e5869825f1e277e29a7f865c196fb711b05
Kayki Letieri <kaykiletieri37@gmail.com>
2024-05-14 13:46:57 -0300

    feat: add mappings to db context

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CSharpAuth.Domain.Entities;
+using CSharpAuth.Infrastructure.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSharpAuth.Infrastructure;
 
@@ -8,8 +10,20 @@ public class ApplicationDbContext : DbContext
     {
     }
 
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<RolePermission> RolePermissions { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfiguration(new UserMapping());
+        modelBuilder.ApplyConfiguration(new RoleMapping());
+        modelBuilder.ApplyConfiguration(new PermissionMapping());
+        modelBuilder.ApplyConfiguration(new UserRoleMapping());
+        modelBuilder.ApplyConfiguration(new RolePermissionMapping());
+
         base.OnModelCreating(modelBuilder);
     }
 }
